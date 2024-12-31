a5d71ac6ef08cc3e44d76018fa5a886e825e7459
mayank <mayank@mahitechllc.com>
2024-12-31 18:56:08 +0530

    init

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const os = require('os');
 
 // Create Express app for serving the HTML client
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 4000;
 
 // Get server IP address
 function getServerIP() {
